a007dece3db4c804ca37ce2c0e5ff27f44e78da7
Janki <Janki@123>
2018-12-11 18:30:57 -0600

    Version 2

--- Disease_outbreak_prediction.py
+++ Disease_outbreak_prediction.py
@@ -1,16 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#List to store the aggregate score for all the patients
 aggregate_score_list = []
-
-def generate_score_for_patients(num):
-    """
-    The function calculates the aggregate score for num (number of patients/sample size) to be considered
-    :param num: number of cases to be considered (cases reported in the real-life scenario)
-    :return: a list of aggregate scores for all the cases.
-    """
-    for i in range(1,num):
+red_zone = []
+def generate_score_for_patients():
+    for i in range(1,100):
         time_diff = np.random.random_integers(0, 28)
         if time_diff < 7:
             time_diff_score = 100
@@ -31,9 +25,17 @@ def generate_score_for_patients(num):
         contagious_score = np.random.choice(virus_type)
 
         aggregate_score = time_diff_score + dist_range_score + contagious_score
+        if aggregate_score >= 250:
+            red_zone.append(1)
+        else:
+            red_zone.append(0)
         aggregate_score_list.append(aggregate_score)
-    return(aggregate_score_list)
+    #plt.plot(aggregate_score_list)
+    #plt.show()
+    return (aggregate_score_list, red_zone)
 
-aggr_score = generate_score_for_patients(100)
-plt.plot(aggr_score)
+agg_scr_list, red_zone_list = generate_score_for_patients()
+plt.plot(agg_scr_list)
 plt.show()
+#print(red_zone_list)
+#plt.show()
