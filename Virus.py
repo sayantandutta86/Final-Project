4271b9bc2b9a782a4aa21e4aecfedba4236aaddb
Janki <Janki@123>
2018-12-14 18:49:53 -0600

    With comments

--- Virus.py
+++ Virus.py
@@ -4,19 +4,19 @@ import random
 
 class virus:
 
-    def __init__(self):
+    def __init__(self, virus_type):
         """ This is the method which intializes all the variables associated with virus class
                 """
-        self.type = random.choice(['C','FC','MC'])
-        self.type_score = 100 #default value
+        self.type = virus_type
+        self.type_score = 0 #default value
 
     def get_type_score(self) -> int:
         """ Assign a pre-defined score based on how contagious the virus is.
         :return: type_score
                 """
-        if self.type == 'C':
+        if self.type == 1:
             self.type_score = 100
-        elif self.type == 'FC':
+        elif self.type == 2:
             self.type_score = 50
         else:
             self.type_score = 10
