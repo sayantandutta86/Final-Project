a60c4aa3c71bc3ab4ee0c373e7cc7366d485f61d
sayedhussain <31064346+sayedhussain@users.noreply.github.com>
2018-12-14 21:42:31 -0600

    exception handling updated

--- ExceptionClass.py
+++ ExceptionClass.py
@@ -1,16 +1,16 @@
-'''
-Created on Nov 24, 2015
-
-@author: sayed.hussain
-'''
+# Custom Exception class to handle in program exceptions
 
 
 class VirusTypeException(Exception):
+    '''This type of exception will occur when the user chooses value other than 1,2 or 3 for virus type when
+    prompted'''
     def __init__(self):
         super().__init__('Enter valid virus type input. Value can be 1, 2 or 3')
 
 
 class PopulationSizeException(Exception):
+    '''This type of exception will occur when the user inputs value less than 0 of negative for the population
+    variable'''
     def __init__(self):
         super().__init__('Population size should be greater than 0')
 
